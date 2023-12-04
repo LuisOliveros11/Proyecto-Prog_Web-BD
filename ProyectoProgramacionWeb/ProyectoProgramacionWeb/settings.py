82d71aeafa06ab11306e3cea72a012c144578d48
LuisOliveros11 <lparada_19@alu.uabcs.mx>
2023-12-04 14:23:22 -0700

    VERSION FINAL-Subida de carpetas de proyecto e imagenes de prueba

--- ProyectoProgramacionWeb/ProyectoProgramacionWeb/settings.py
+++ ProyectoProgramacionWeb/ProyectoProgramacionWeb/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ProyectoProgramacionWeb',
         'USER': 'root',
-        'PASSWORD': 'Bahialucila219',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
